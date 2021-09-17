ae3e644d2c997c644568f4f4ed72b4a0a62c43d6
Deasde34 <jorgeezp2008@hotmail.com>
2021-09-16 21:39:27 -0700

    bruh

--- src/Menu.cc
+++ src/Menu.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include "Stack.hh"
+#include "Menu.hh"
 
 Stack::Stack()
 {
@@ -63,4 +64,30 @@ void Stack::Print()
       current = current->next;
     }
   }
+}
+
+
+
+Menu::Menu(std::string material, std::string crockery, int howMuch)
+{
+  this->material = material;
+  this->crockery = crockery;
+  this->howMuch = howMuch;
+}
+
+Menu::~Menu()
+{
+}
+
+std::string Menu::GetMaterial() const
+{
+  return material;
+}
+std::string Menu::GetCrockery() const
+{
+  return crockery;
+}
+int Menu::GetHowMuch() const
+{
+  return howMuch;
 }
