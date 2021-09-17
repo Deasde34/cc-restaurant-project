bfb6652de159b9aac86f2d83aefda80e8452efba
Deasde34 <jorgeezp2008@hotmail.com>
2021-09-16 17:33:07 -0700

    final

--- include/Menu.hh
+++ include/Menu.hh
@@ -11,7 +11,7 @@ private:
 public:
   Menu(std::string material, std::string crockery,  int howMuch);
   ~Menu();
-  std::string GetType() const;
+  std::string GetMaterial() const;
   std::string GetCrockery() const;
   int GetHowMuch() const;
 };
